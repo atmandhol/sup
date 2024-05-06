da6e652b4e9637fa6bb5d6792e2bb389635bcbc1
Atman Dhol <atmandhol@hotmail.com>
2024-05-05 23:56:59 -0400

    feat: Add name filter

--- widgets/run_list.py
+++ widgets/run_list.py
@@ -1,7 +1,8 @@
 from textual.app import ComposeResult
 from textual.binding import Binding
+from textual.containers import Vertical
 from textual.reactive import Reactive
-from textual.widgets import Static, DataTable
+from textual.widgets import Static, DataTable, Input
 from k8s.k8s import KubectlCmd
 from rich.text import Text
 
@@ -10,6 +11,7 @@ class RunList(Static):
     """Run list"""
 
     run_data = Reactive(list())
+    filter_string = Reactive(str())
 
     def __init__(self):
         super().__init__()
@@ -19,7 +21,9 @@ class RunList(Static):
     ]
 
     def compose(self) -> ComposeResult:
-        yield DataTable(id="runDataTable")
+        with Vertical():
+            yield Input(id="filterInput")
+            yield DataTable(id="runDataTable")
 
     def on_mount(self) -> None:
         table = self.query_one(DataTable)
@@ -35,17 +39,26 @@ class RunList(Static):
             "message"
         )
         table.add_columns(*run_list)
-        self.set_interval(10, self.update_run_data)
+        self.set_interval(30, self.update_run_data)
         self.update_run_data()
+
+    def on_input_changed(self, input):
+        self.filter_string = input.value
 
     def update_run_data(self):
         self.run_data = KubectlCmd.get_run_list()
+
+    def watch_filter_string(self):
+        self.watch_run_data()
 
     def watch_run_data(self):
         table = self.query_one(DataTable)
         current_pos = table.cursor_row
         table.clear()
         for run in self.run_data:
+            if self.filter_string and self.filter_string not in (str(run.get("metadata").get("labels").get("supply-chain.apps.tanzu.vmware.com/workload-name")) + "/" + str(run.get("metadata").get("name"))):
+                continue
+
             styled_row = list()
 
             # Namespace
