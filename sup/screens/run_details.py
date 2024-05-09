734c13b9cc8937f6810865a8460ec1ea4959d33c
Atman Dhol <atmandhol@hotmail.com>
2024-05-09 17:38:57 -0400

    feat: Added first pass of stage details page

--- sup/screens/run_details.py
+++ sup/screens/run_details.py
@@ -1,10 +1,24 @@
 import emoji
+import os
+import yaml
 from rich.text import Text
 from textual.app import ComposeResult
 from textual.containers import Horizontal
 from textual.reactive import Reactive
 from textual.screen import Screen
-from textual.widgets import Footer, Static, Tree, Label, TabbedContent, TabPane
+from textual.widgets import (
+    Footer,
+    Static,
+    Tree,
+    Label,
+    TabbedContent,
+    TabPane,
+    MarkdownViewer,
+)
+
+# noinspection PyProtectedMember
+from textual.widgets._tree import TreeNode
+
 from sup.k8s.k8s import KubectlCmd
 
 
@@ -16,6 +30,8 @@ class RunDetail(Screen):
     def __init__(self, run: str, namespace: str):
         super().__init__()
         self.run = run
+        # noinspection PyTypeChecker
+        self.selected_stage: TreeNode = None
         self.namespace = namespace
         self.refresh_time_in_sec = 5
 
@@ -31,7 +47,11 @@ class RunDetail(Screen):
             with Static(id="data_panel"):
                 with TabbedContent():
                     with TabPane("Details", id="detailsTab"):
-                        pass
+                        yield MarkdownViewer(
+                            markdown="Select a stage to view the details",
+                            show_table_of_contents=False,
+                            id="markdownStageDetail",
+                        )
                     with TabPane("Logs", id="logsTab"):
                         pass
                     with TabPane("YAML", id="yamlTab"):
@@ -42,31 +62,130 @@ class RunDetail(Screen):
         self.set_interval(self.refresh_time_in_sec, self.update_run_details)
         self.update_run_details()
 
+    def on_tree_node_selected(self, widget) -> None:
+        if widget.node.data:
+            self.selected_stage = widget.node
+            self.populate_stage_details()
+
+    def populate_stage_details(self):
+        # noinspection PyBroadException
+        try:
+            if self.selected_stage:
+                path = os.path.dirname(os.path.abspath(__file__)).replace(
+                    "/screens", f"/templates/stage_detail.md"
+                )
+                tpl = open(path, "r").read()
+                stage = self.selected_stage.data.get("run_spec_stage")
+                spec = self.selected_stage.data.get("status_stage")
+                # noinspection PyTypeChecker
+                markdown: MarkdownViewer = self.query_one("#markdownStageDetail")
+                markdown.show_table_of_contents = True
+                markdown.show_vertical_scrollbar = True
+                markdown.show_horizontal_scrollbar = False
+                final_data = (
+                    str(tpl)
+                    .replace("%component_name", stage.get("componentRef").get("name"))
+                    .replace("%namespace", stage.get("componentRef").get("namespace"))
+                    .replace(
+                        "%outputs",
+                        yaml.dump(stage.get("outputs", "No Outputs to report")),
+                    )
+                    .replace(
+                        "%pipelinerun_name",
+                        spec.get("pipelineRun").get("ref").get("name")
+                        if spec.get("pipelineRun")
+                        else "PipelineRun not created",
+                    )
+                    .replace(
+                        "%pipelinerun_ns",
+                        spec.get("pipelineRun").get("ref").get("namespace")
+                        if spec.get("pipelineRun")
+                        else "PipelineRun not created",
+                    )
+                    .replace(
+                        "%pipeline_passed",
+                        str(stage.get("pipeline").get("passed", "Did not run"))
+                        if stage.get("pipeline")
+                        and stage.get("pipeline").get("passed") is not None
+                        else "Stage did not run/finish",
+                    )
+                    .replace(
+                        "%pipeline_start",
+                        stage.get("pipeline").get("started", "Did not run")
+                        if stage.get("pipeline")
+                        and stage.get("pipeline").get("started")
+                        else "Stage did not run/finish",
+                    )
+                    .replace(
+                        "%pipeline_end",
+                        stage.get("pipeline").get("completed", "Did not run")
+                        if stage.get("pipeline")
+                        and stage.get("pipeline").get("completed")
+                        else "Stage did not run/finish",
+                    )
+                    .replace(
+                        "%pipeline_results",
+                        yaml.dump(
+                            stage.get("pipeline").get("results", "No Results to Show")
+                        )
+                        if stage.get("pipeline")
+                        and stage.get("pipeline").get("passed") is not None
+                        else "No Results to show",
+                    )
+                    .replace(
+                        "%pipeline_message",
+                        stage.get("pipeline").get("message", "Did not run")
+                        if stage.get("pipeline")
+                        and stage.get("pipeline").get("message")
+                        else "Stage did not run/finish",
+                    )
+                )
+                markdown.document.update(final_data)
+
+        except Exception as err:
+            self.notify(
+                f"Sup was unable to get Stage details from the cluster. Error {err}",
+                title="Refresh Error",
+                severity="error",
+                timeout=self.refresh_time_in_sec,
+            )
+
     def populate_stage_tree(self):
         # noinspection PyTypeChecker
         tree: Tree = self.query_one("#stagesTree")
         tree.clear()
         stages_node = tree.root
         stages_node.expand()
+        ct = 0
         for stage in (
             self.run_details.get("status").get("workloadRun").get("spec").get("stages")
         ):
             if not stage.get("pipeline"):
                 ej = emoji.emojize(":white_circle: ")
-            elif (
-                stage.get("pipeline").get("started")
-                and not stage.get("pipeline").get("completed")
+                spec = {}
+            elif stage.get("pipeline").get("started") and not stage.get("pipeline").get(
+                "completed"
             ):
                 ej = emoji.emojize(":blue_circle: ")
+                spec = self.run_details.get("status").get("stages", [])[ct]
             elif (
                 stage.get("pipeline").get("passed")
-                and stage.get("pipeline").get("passed") == True
+                and stage.get("pipeline").get("passed") is True
             ):
                 ej = emoji.emojize(":green_circle: ")
+                spec = self.run_details.get("status").get("stages", [])[ct]
             else:
                 ej = emoji.emojize(":red_circle: ")
+                spec = self.run_details.get("status").get("stages", [])[ct]
 
-            stages_node.add_leaf(ej + stage.get("name"), stage)
+            stages_node.add_leaf(
+                ej + stage.get("name"),
+                {
+                    "run_spec_stage": stage,
+                    "status_stage": spec,
+                },
+            )
+            ct += 1
 
     def populate_top_bar(self):
         self.query_one("#runLabel").renderable = (
@@ -135,7 +254,7 @@ class RunDetail(Screen):
             )
 
     def watch_run_details(self):
-        # Do something when the run data changes
         self.populate_top_bar()
         self.populate_stage_tree()
-        pass
+        if self.selected_stage:
+            self.populate_stage_details()
---
+++ sup/templates/stage_detail.md
@@ -0,0 +1,29 @@
+## %component_name
+
+### Overview
+```yaml
+namespace: %namespace
+```
+
+### Outputs
+```yaml
+Outputs:
+%outputs
+```
+
+### Pipeline
+```yaml
+Pipeline Run: %pipelinerun_name
+Pipeline Run Namespace: %pipelinerun_ns
+Passed: %pipeline_passed
+Start: %pipeline_start
+Completed: %pipeline_end
+Results:
+%pipeline_results
+Message: %pipeline_message
+```
+
+### Resumptions
+```yaml
+%resumptions
+```
