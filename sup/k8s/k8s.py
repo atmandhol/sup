41e8c6fb77ac7a26f525d7bd7adb243b82604a5b
Atman Dhol <atmandhol@hotmail.com>
2024-05-16 10:45:07 -0400

    feat: Add code for run list filters

--- sup/k8s/k8s.py
+++ sup/k8s/k8s.py
@@ -22,9 +22,33 @@ class KubectlCmd:
         return process, out, err
 
     @staticmethod
-    def get_run_list():
+    def get_run_list(chains: list = None, statuses: list = None, latest=True):
         _, out, _ = KubectlCmd.run("get all-runs -A -ojson")
-        return json.loads(out).get("items")
+        run_list = json.loads(out).get("items")
+        filtered_run_list = list()
+        for run in run_list:
+            latest_check_passed = False
+            chain_check_passed = False
+            status_check_passed = False
+
+            if not latest:
+                latest_check_passed = True
+            elif latest and KubectlCmd.is_latest(run, run_list):
+                latest_check_passed = True
+
+            if not chains:
+                chain_check_passed = True
+            elif KubectlCmd.belongs_to_chains(run, chains):
+                chain_check_passed = True
+
+            if not statuses:
+                status_check_passed = True
+            elif KubectlCmd.belongs_to_statuses(run, chains):
+                status_check_passed = True
+
+            if latest_check_passed and chain_check_passed and status_check_passed:
+                filtered_run_list.append(run)
+        return filtered_run_list
 
     @staticmethod
     def get_run_detail(run: str, namespace: str):
@@ -41,3 +65,37 @@ class KubectlCmd:
         # Add [{{color .PodColor .PodName}}] after message to add stage
         _, out, _ = KubectlCmd.stern_run(cmd)
         return out.decode(), cmd
+
+    @staticmethod
+    def is_latest(run, run_list):
+        workload = (
+            run.get("metadata")
+            .get("labels")
+            .get("supply-chain.apps.tanzu.vmware.com/workload-name")
+        )
+        created_at_timestamp = run.get("metadata").get("creationTimestamp")
+        for r in run_list:
+            if (
+                r.get("metadata")
+                .get("labels")
+                .get("supply-chain.apps.tanzu.vmware.com/workload-name")
+                != workload
+            ):
+                continue
+            if r.get("metadata").get("creationTimestamp") > created_at_timestamp:
+                return False
+        return True
+
+    @staticmethod
+    def belongs_to_chains(run, chain_list):
+        chain = (
+            run.get("metadata")
+            .get("labels")
+            .get("supply-chain.apps.tanzu.vmware.com/workload-kind")
+        )
+        return chain in chain_list
+
+    @staticmethod
+    def belongs_to_statuses(run, status_list):
+        status = run.get("status").get("conditions")[1].get("reason")
+        return status in status_list
